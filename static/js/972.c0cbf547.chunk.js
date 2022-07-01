7865eddb87daba304ee7549523ef04f79a7512dd
amritach222 <amritach222@gmail.com>
2022-07-01 11:17:11 +0545

    Updates

---
+++ static/js/972.c0cbf547.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdiabetestrackerapp=self.webpackChunkdiabetestrackerapp||[]).push([[972],{87972:function(e,a,i){i.r(a);i(72791);var r=i(80184);a.default=function(){return(0,r.jsx)("div",{children:"This is Prediction page"})}}}]);
+//# sourceMappingURL=972.c0cbf547.chunk.js.map
